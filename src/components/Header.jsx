77a369ec981d486d0e3a75d3b8252aab91d5b207
PeriYumYum <perilin124@gmail.com>
2023-04-21 21:38:38 +0800

    Add components, remove unnecessary files

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AccessAlarmIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
